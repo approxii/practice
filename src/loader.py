e0e18508bb60deb0754c404271b4431d28cda257
approxii <118077669+approxii@users.noreply.github.com>
2024-06-03 23:23:15 +0300

    Delete src directory

--- src/loader.py
+++
@@ -1,7 +0,0 @@
-from pptx import Presentation
-
-class DataLoader:
-    #Класс для загрузки данных из PPTX файлов.
-    def load_presentation(self, path):
-        #Загружает PPTX презентацию.
-        return Presentation(path)
