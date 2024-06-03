10c6a5a141cad868e550597fa7301f13aac92fd4
approxii <118077669+approxii@users.noreply.github.com>
2024-06-04 00:04:04 +0300

    Update depends.py

--- core/api/router/powerpoint/depends.py
+++ core/api/router/powerpoint/depends.py
@@ -1,4 +1,4 @@
-from core.services.powerpoint import parser as Service
+from core.services.powerpoint import PowerPointService as Service
 
 async def get_service() -> Service:
     return Service()
