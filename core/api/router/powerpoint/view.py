d4a0ea46d34501b9c55257082b4dad0a47f8c264
approxii <118077669+approxii@users.noreply.github.com>
2024-06-03 23:32:26 +0300

    Add files via upload

---
+++ core/api/router/powerpoint/view.py
@@ -0,0 +1,18 @@
+from io import BytesIO
+from fastapi import FastAPI
+from fastapi import Depends, File, UploadFile
+from core.services.powerpoint import parser as Service
+from core.api.router.powerpoint.depends import get_service
+
+app: FastAPI = FastAPI(title="Microsoft documents generate/analyze")
+
+@app.post("/update/")
+async def upload_file_and_dict(
+    file: UploadFile = File(...),
+    service: Service = Depends(get_service),
+):
+    contents = await file.read()
+    service.load(BytesIO(contents))
+    res = service.analyze()
+
+    return res
